3aa2d052c0189207bd2c62ae99499936c09c1be7
piyush97 <me@piyushmehta.com>
2022-04-17 14:28:44 -0400

    fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="flex justify-between">
+    <header className="flex justify-between p-5 mx-auto max-w-7xl">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
