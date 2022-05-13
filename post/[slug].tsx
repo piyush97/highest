611bcc23b171c6e252e7c33ef65772cdccde7e87
piyush97 <me@piyushmehta.com>
2022-05-13 14:48:41 -0400

    posts

---
+++ post/[slug].tsx
@@ -0,0 +1,61 @@
+import { GetStaticProps } from 'next'
+import React, { Props } from 'react'
+import Header from '../components/Header'
+import { sanityClient } from '../sanity'
+import { Post } from '../typings'
+
+interface Props {
+  post: Post
+}
+
+const Post = ({ post }: Props) => {
+  console.log(post)
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
+
+export default Post
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+            _id,
+            slug {
+                current
+            }
+    }]`
+
+  const posts = await sanityClient.fetch(query)
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]]{
+            _id,
+            title,
+            createdAt,
+            author-> {
+                name,
+                image},
+            description,
+            mainImage,
+            slug
+        }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+  if (!post) {
+    return { notFound: true }
+  }
+  return { props: { post } }
+}
