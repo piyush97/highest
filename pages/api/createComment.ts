c26c309b2267c4a20fb003db9fc64955edd197c4
piyush97 <me@piyushmehta.com>
2022-05-13 18:25:33 -0400

    createComment api

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,9 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { createClient } from 'next-sanity'
 
-export default function createComment(
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT,
+  token: process.env.SANITY_API_TOKEN,
+  useCdn: process.env.NODE_ENV === 'production',
+}
+
+const client = createClient(config)
+
+export default async function createComment(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { _id, name, email, comment } = JSON.parse(req.body)
+  try {
+    await client.create({
+      _type: 'comment',
+      post: {
+        _type: 'reference',
+        _ref: _id,
+      },
+      name,
+      email,
+      comment,
+    })
+  } catch (err) {
+    return res.status(500).json({ message: "Couldn't submit comment", err })
+  }
+  console.log('Comment Submitted')
+  res.status(200).json({ message: 'comment submitted' })
 }
